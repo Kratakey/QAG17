32fc2d021d0ca4cc8aec3940066a566f971e63a6
Kratakey <Kratakey@gmail.com>
2021-12-21 20:21:56 +0300

    fix

--- src/test/java/ReqresinTests.java
+++ src/test/java/ReqresinTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
-public class reqresinTests {
+public class ReqresinTests {
 
     @Test
     void userCreate() {
